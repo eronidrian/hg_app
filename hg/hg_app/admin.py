1206202064537030c839cb13fb733f2a1f49a610
eronidrian <101004991+eronidrian@users.noreply.github.com>
2022-04-05 21:07:35 +0200

    order!

--- hg/hg_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Package, Point
-
-admin.site.register(Package)
-admin.site.register(Point)
--- hg/hg_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HgAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hg_app'
